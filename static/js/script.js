2baf4a2a7cfbea071e4e93afb7fb10a9cff25b86
Gary Hughes <gary.hughes87@virginmedia.com>
2021-02-04 10:30:01 +0000

    Added categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right", draggable: true });
     $('.collapsible.expandable').collapsible({ accordion: false });
     $('.tooltipped').tooltip();
+    $('select').formSelect();
     $('.datepicker').datepicker({
         format: "dd mmmmm, yyyy", 
         yearRange: 3,
